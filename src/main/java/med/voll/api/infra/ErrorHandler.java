6fe1a5fa538c5a3a2758b1766a47adaf3eaad3f9
Guilherme Cardoso <gcs200206@gmail.com>
2025-04-06 21:12:15 -0300

    feat: create method handleError404

--- src/main/java/med/voll/api/infra/ErrorHandler.java
+++ src/main/java/med/voll/api/infra/ErrorHandler.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +13,18 @@ public class ErrorHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity handleError404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleError400(MethodArgumentNotValidException ex) {
+        var error = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(error.stream().map(ValidationErrorDTO::new).toList());
+    }
+
+    private record ValidationErrorDTO(String field, String message) {
+        public ValidationErrorDTO(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 
 }
