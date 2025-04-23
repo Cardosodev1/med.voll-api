29b637c6c72dcc4b0569fc3c5e292615bc64cfb6
Guilherme Cardoso <gcs200206@gmail.com>
2025-04-23 17:40:31 -0300

    refactor: update UserController

--- src/main/java/med/voll/api/controller/UserController.java
+++ src/main/java/med/voll/api/controller/UserController.java
@@ -2,6 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.dto.UserDTO;
+import med.voll.api.domain.entity.user.User;
+import med.voll.api.infra.security.TokenDTO;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,12 +21,16 @@ public class UserController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity logIn(@RequestBody @Valid UserDTO userDTO) {
-        var token = new UsernamePasswordAuthenticationToken(userDTO.login(), userDTO.senha());
-        var authentication = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(userDTO.login(), userDTO.senha());
+        var authentication = manager.authenticate(authenticationToken);
+        var tokenJWT = tokenService.getToken((User) authentication.getPrincipal());
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new TokenDTO(tokenJWT));
     }
 
 }
