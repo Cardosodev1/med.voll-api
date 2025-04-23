75c34448437ebe214f30d93fc00584ca9b656c8d
Guilherme Cardoso <gcs200206@gmail.com>
2025-04-23 17:40:15 -0300

    feat: create TokenDTO, TokenService

---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,37 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.entity.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.secutiry.token.secret}")
+    private String secret;
+
+    public String getToken(User user) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(expirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException e){
+            throw new RuntimeException("erro ao gerrar token jwt", e);
+        }
+    }
+
+    private Instant expirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
