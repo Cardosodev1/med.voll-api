e72afc5f0342b18408a03e7760264c095529e478
Guilherme Cardoso <gcs200206@gmail.com>
2025-05-22 18:49:30 -0300

    refactor: update ConsultationDetailsDTO

--- src/main/java/med/voll/api/domain/dto/consultation/ConsultationDetailsDTO.java
+++ src/main/java/med/voll/api/domain/dto/consultation/ConsultationDetailsDTO.java
@@ -1,9 +1,16 @@
 package med.voll.api.domain.dto.consultation;
 
+import med.voll.api.domain.entity.consultation.Consultation;
+
 import java.time.LocalDateTime;
 
 public record ConsultationDetailsDTO(Long id,
                                      Long idDoctor,
                                      Long idPatient,
                                      LocalDateTime dateConsultation) {
+
+    public ConsultationDetailsDTO(Consultation consultation) {
+        this(consultation.getId(), consultation.getDoctor().getId(), consultation.getPatient().getId(), consultation.getDateConsultation());
+    }
+
 }
