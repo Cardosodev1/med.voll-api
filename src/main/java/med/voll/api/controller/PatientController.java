b6a783fe233466410ec24cb2fc0c32e48e40b95b
Guilherme Cardoso <gcs200206@gmail.com>
2025-05-23 12:58:28 -0300

    refactor: add bearer-key

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.dto.patient.PatientDTO;
 import med.voll.api.domain.dto.patient.PatientDetailsDTO;
@@ -18,6 +19,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/patients")
+@SecurityRequirement(name = "bearer-key")
 public class PatientController {
 
     @Autowired
