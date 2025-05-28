d264f77cf636ee021caf55d42776087c588e8f04
Guilherme Cardoso <gcs200206@gmail.com>
2025-05-28 18:34:24 -0300

    refactor: update ConsultationController

--- src/main/java/med/voll/api/controller/ConsultationController.java
+++ src/main/java/med/voll/api/controller/ConsultationController.java
@@ -2,16 +2,14 @@ package med.voll.api.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
+import med.voll.api.domain.dto.consultation.ConsultationCancellationDTO;
 import med.voll.api.domain.dto.consultation.ConsultationDTO;
 import med.voll.api.domain.dto.consultation.ConsultationDetailsDTO;
 import med.voll.api.domain.service.ConsultationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/consultations")
@@ -26,6 +24,13 @@ public class ConsultationController {
     public ResponseEntity schedule(@RequestBody @Valid ConsultationDTO dto) {
         var details = service.schedule(dto);
         return ResponseEntity.ok(details);
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity cancel(@RequestBody @Valid ConsultationCancellationDTO dto) {
+        service.cancel(dto);
+        return ResponseEntity.noContent().build();
     }
 
 }
